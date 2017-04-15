fb9d8d31ad40d6fd081d3b8fb3fe1a1ae11ae90a
mmirzay <mirzay.mohsen@gmail.com>
2017-04-15 15:50:52 +0430

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['mmirzay'],
-    :password       => ENV['momo12345678'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
